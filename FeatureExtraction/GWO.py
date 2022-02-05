5799db29ae7c5228bf7695d403dc1e3c7a8ff2a6
Lukas Kleybolte <mozartus@daenerys.multimedia.hs-augsburg.de>
2022-02-05 12:47:27 +0100

    :gear: add Grey Wolf optimizer

---
+++ FeatureExtraction/GWO.py
@@ -0,0 +1,105 @@
+import multiprocessing
+from concurrent.futures import ProcessPoolExecutor
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+from FeatureExtraction.GWO_helper import jfs
+from Utils.Constants import FINAL_DATASET_PATH_GWO, PREPROCESSED_DATA_PATH_FS, SAVE_GWO_CHANNELS_PATH
+
+CLASSIFY_TYPE = "Valence"
+N = 10  # number of particles
+T = 10  # maximum number of iterations
+COMP = 20
+
+
+def use_gwo(participant_list: [int] = range(1, 10), classify_type: str = "Arousal", n_particle: int = 128,
+            n_iterations: int = 32, components: int = 20, n_cores: int = multiprocessing.cpu_count()):
+    global CLASSIFY_TYPE, N, T, COMP
+    COMP = components
+    CLASSIFY_TYPE = classify_type
+    N = n_particle
+    T = n_iterations
+    print(
+        f"Run GWO channel selection method with {CLASSIFY_TYPE} and with {N} particles and with {T} max iterations on {n_cores} cores")
+
+    x_train = []
+    y_train = []
+    x_test = []
+    y_test = []
+    backup = []
+
+    save_path_data_training = Path(FINAL_DATASET_PATH_GWO, "data_training.npy")
+    save_path_label_training = Path(FINAL_DATASET_PATH_GWO, "label_training.npy")
+    save_path_data_testing = Path(FINAL_DATASET_PATH_GWO, "data_testing.npy")
+    save_path_label_testing = Path(FINAL_DATASET_PATH_GWO, "label_testing.npy")
+    FINAL_DATASET_PATH_GWO.mkdir(exist_ok=True)
+    SAVE_GWO_CHANNELS_PATH.mkdir(exist_ok=True)
+    with ProcessPoolExecutor(max_workers=n_cores) as executor:
+        for label, zx, p, ch, acc in executor.map(exec_gwo, participant_list):
+            print(f"Finish participant {p} with {len(ch)} channels")
+            ch = list(ch)
+            acc = list(acc)
+            backup.append([p, ch, len(ch), acc])
+            for i in range(0, zx.shape[0]):
+                if i % 4 == 0:
+                    x_test.append(zx[i].reshape(zx.shape[1] * zx.shape[2], ))
+                    y_test.append(label[i])
+                else:
+                    x_train.append(zx[i].reshape(zx.shape[1] * zx.shape[2], ))
+                    y_train.append(label[i])
+
+    pd.DataFrame(backup, columns=["participant", "channels", "n_channels", "channel_acc"], index=None).to_csv(
+        Path(SAVE_GWO_CHANNELS_PATH, f"gwo_channels_{classify_type}.csv"), index=False)
+
+    np.save(str(save_path_data_training), np.array(x_train), allow_pickle=True, fix_imports=True)
+    np.save(str(save_path_label_training), np.array(y_train), allow_pickle=True, fix_imports=True)
+
+    np.save(str(save_path_data_testing), np.array(x_test), allow_pickle=True, fix_imports=True)
+    np.save(str(save_path_label_testing), np.array(y_test), allow_pickle=True, fix_imports=True)
+
+
+def exec_gwo(participant):
+    num_channel = 32
+    num_frequencies = 5
+
+    with open(Path(PREPROCESSED_DATA_PATH_FS, f"Participant_{participant}.npy"), "rb") as file:
+        sub = np.load(file, allow_pickle=True)
+        data = []
+        label = []
+        for i in range(0, sub.shape[0]):
+            data.append(np.array(sub[i][0]))
+            label.append(np.array(sub[i][1]))
+        data = np.array(data)
+        label = np.array(label)
+    x = data.transpose((1, 0, 2)).reshape(num_channel, -1).transpose((1, 0))
+    if CLASSIFY_TYPE.lower() == "arousal":
+        y = np.repeat(label[:, 0], num_frequencies)
+    else:
+        y = np.repeat(label[:, 1], num_frequencies)
+    # split data into train & validation (70 -- 30)
+    xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size=0.3, stratify=y)
+    fold = {'xt': xtrain, 'yt': ytrain, 'xv': xtest, 'yv': ytest}
+    # parameter  # k-value in KNN
+    w = 0.9
+    c1 = 2
+    c2 = 2
+    opts = {'fold': fold, 'N': N, 'T': T, 'w': w, 'c1': c1, 'c2': c2}
+    fmdl = jfs(x, y, opts, COMP)
+    sf = fmdl['sf']
+
+    x = pd.DataFrame(x)
+    data_new = x[sf].to_numpy()
+    z = []
+    for i in data_new.transpose(1, 0):
+        z.append(i.reshape(-1, num_frequencies))
+    zx = np.array(z).transpose((1, 0, 2))
+    return label, zx, participant, sf, fmdl['df']
+
+
+if __name__ == '__main__':
+    print(multiprocessing.cpu_count())
+    use_gwo(participant_list=range(1, 10), classify_type="Arousal", n_particle=2, n_iterations=1, components=20,
+            n_cores=1)
---
+++ FeatureExtraction/GWO_helper.py
@@ -0,0 +1,189 @@
+import numpy as np
+from numpy.random import rand
+from sklearn.svm import SVC
+
+
+def error_rate(x, opts):
+    # parameters
+    fold = opts['fold']
+    xt = fold['xt']
+    yt = fold['yt']
+    xv = fold['xv']
+    yv = fold['yv']
+
+    # Number of instances
+    num_train = np.size(xt, 0)
+    num_valid = np.size(xv, 0)
+    # Define selected features
+    xtrain = xt[:, x == 1]
+    ytrain = yt.reshape(num_train)  # Solve bug
+    xvalid = xv[:, x == 1]
+    yvalid = yv.reshape(num_valid)  # Solve bug
+    # Training
+    mdl = SVC(max_iter=1000, gamma='auto', random_state=10, kernel="rbf")
+    mdl.fit(xtrain, ytrain)
+    # Prediction
+    ypred = mdl.predict(xvalid)
+    acc = np.sum(yvalid == ypred) / num_valid
+    error = 1 - acc
+
+    return error
+
+
+# Error rate & Feature size
+def Fun(x, opts):
+    # Parameters
+    alpha = 0.99
+    beta = 1 - alpha
+    # Original feature size
+    max_feat = len(x)
+    # Number of selected features
+    num_feat = np.sum(x == 1)
+    # Solve if no feature selected
+    if num_feat == 0:
+        cost = 1
+    else:
+        # Get error rate
+        error = error_rate(x, opts)
+        # Objective function
+        cost = alpha * error + beta * (num_feat / max_feat)
+
+    return cost
+
+
+def init_position(lb, ub, N, dim):
+    X = np.zeros([N, dim], dtype='float')
+    for i in range(N):
+        for d in range(dim):
+            X[i, d] = lb[0, d] + (ub[0, d] - lb[0, d]) * rand()
+
+    return X
+
+
+def binary_conversion(X, thres, N, dim):
+    Xbin = np.zeros([N, dim], dtype='int')
+    for i in range(N):
+        for d in range(dim):
+            if X[i, d] > thres:
+                Xbin[i, d] = 1
+            else:
+                Xbin[i, d] = 0
+
+    return Xbin
+
+
+def boundary(x, lb, ub):
+    if x < lb:
+        x = lb
+    if x > ub:
+        x = ub
+
+    return x
+
+
+def jfs(xtrain, ytrain, opts, resulting_channels):
+    # Parameters
+    ub = 1
+    lb = 0
+    thres = 0.5
+
+    N = opts['N']
+    max_iter = opts['T']
+
+    # Dimension
+    dim = np.size(xtrain, 1)
+    if np.size(lb) == 1:
+        ub = ub * np.ones([1, dim], dtype='float')
+        lb = lb * np.ones([1, dim], dtype='float')
+
+    # Initialize position
+    X = init_position(lb, ub, N, dim)
+
+    # Binary conversion
+    Xbin = binary_conversion(X, thres, N, dim)
+
+    # Fitness at first iteration
+    fit = np.zeros([N, 1], dtype='float')
+    Xalpha = np.zeros([1, dim], dtype='float')
+    Xbeta = np.zeros([1, dim], dtype='float')
+    Xdelta = np.zeros([1, dim], dtype='float')
+    Falpha = float('inf')
+    Fbeta = float('inf')
+    Fdelta = float('inf')
+
+    for i in range(N):
+        fit[i, 0] = Fun(Xbin[i, :], opts)
+        if fit[i, 0] < Falpha:
+            Xalpha[0, :] = X[i, :]
+            Falpha = fit[i, 0]
+
+        if fit[i, 0] < Fbeta and fit[i, 0] > Falpha:
+            Xbeta[0, :] = X[i, :]
+            Fbeta = fit[i, 0]
+
+        if fit[i, 0] < Fdelta and fit[i, 0] > Fbeta and fit[i, 0] > Falpha:
+            Xdelta[0, :] = X[i, :]
+            Fdelta = fit[i, 0]
+
+    # Pre
+    curve = np.zeros([1, max_iter], dtype='float')
+    t = 0
+
+    curve[0, t] = Falpha.copy()
+    t += 1
+
+    while t < max_iter:
+        # Coefficient decreases linearly from 2 to 0
+        a = 2 - t * (2 / max_iter)
+
+        for i in range(N):
+            for d in range(dim):
+                # Parameter C (3.4)
+                C1 = 2 * rand()
+                C2 = 2 * rand()
+                C3 = 2 * rand()
+                # Compute Dalpha, Dbeta & Ddelta (3.5)
+                Dalpha = abs(C1 * Xalpha[0, d] - X[i, d])
+                Dbeta = abs(C2 * Xbeta[0, d] - X[i, d])
+                Ddelta = abs(C3 * Xdelta[0, d] - X[i, d])
+                # Parameter A (3.3)
+                A1 = 2 * a * rand() - a
+                A2 = 2 * a * rand() - a
+                A3 = 2 * a * rand() - a
+                # Compute X1, X2 & X3 (3.6)
+                X1 = Xalpha[0, d] - A1 * Dalpha
+                X2 = Xbeta[0, d] - A2 * Dbeta
+                X3 = Xdelta[0, d] - A3 * Ddelta
+                # Update wolf (3.7)
+                X[i, d] = (X1 + X2 + X3) / 3
+                # Boundary
+                X[i, d] = boundary(X[i, d], lb[0, d], ub[0, d])
+
+        # Binary conversion
+        Xbin = binary_conversion(X, thres, N, dim)
+
+        # Fitness
+        for i in range(N):
+            fit[i, 0] = Fun(Xbin[i, :], opts)
+            if fit[i, 0] < Falpha:
+                Xalpha[0, :] = X[i, :]
+                Falpha = fit[i, 0]
+
+            if fit[i, 0] < Fbeta and fit[i, 0] > Falpha:
+                Xbeta[0, :] = X[i, :]
+                Fbeta = fit[i, 0]
+
+            if fit[i, 0] < Fdelta and fit[i, 0] > Fbeta and fit[i, 0] > Falpha:
+                Xdelta[0, :] = X[i, :]
+                Fdelta = fit[i, 0]
+
+        curve[0, t] = Falpha.copy()
+        t += 1
+
+    # Best feature subset
+    x_best = Xalpha[0].argsort()[-resulting_channels:][::-1]
+    num_feat = len(x_best)
+    # Create dictionary
+    gwo_data = {'sf': x_best, 'c': curve, 'nf': num_feat}
+
+    return gwo_data
