1b50585653acc31a9d8fcc99ad8f00f2ce8e1ce9
Lukas Kleybolte <mozartus@daenerys.multimedia.hs-augsburg.de>
2022-02-05 12:50:54 +0100

    :gear: add Grey Wolf optimizer

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from pickle import dump
 import matplotlib.pyplot as plt
 import typer
 
+from FeatureExtraction.GWO import use_gwo
 from FeatureExtraction.MRMR import use_mrmr
 from FeatureExtraction.PCA import build_dataset_with_pca
 from FeatureExtraction.PSO import use_pso
@@ -21,7 +22,7 @@ from Utils.DataHandler import LoadData
 
 
 def main(classify_type: str = typer.Argument(..., help="The classification Type:\t Arousal or Valence"),
-         fs: str = typer.Argument(..., help="The channel selection algorithm:\t PCA, MRMR, PSO, NONE"),
+         fs: str = typer.Argument(..., help="The channel selection algorithm:\t PCA, MRMR, PSO, GWO, NONE"),
          overwrite: bool = typer.Argument(...,
                                           help="If you want to build the dataset from scratch and overwrite all the previous files:\t True, False"),
          gpu: str = typer.Argument(...,
@@ -47,6 +48,7 @@ def main(classify_type: str = typer.Argument(..., help="The classification Type:
     fs_pca = False
     fs_mrmr = False
     fs_pso = False
+    fs_gwo = False
     classify_name = classify_type.lower()
 
     if fs != "":
@@ -58,6 +60,8 @@ def main(classify_type: str = typer.Argument(..., help="The classification Type:
         fs_mrmr = True
     elif fs.lower() == "pso":
         fs_pso = True
+    elif fs.lower() == "gwo":
+        fs_gwo = True
 
     SAVED_MODEL_GRAPH_PATH.mkdir(exist_ok=True)
     SAVE_TRAINED_MODEL_PATH.mkdir(exist_ok=True)
@@ -73,19 +77,22 @@ def main(classify_type: str = typer.Argument(..., help="The classification Type:
                        step_size=16,
                        sample_rate=128,
                        overwrite=overwrite,
-                       fs=fs_pca or fs_mrmr or fs_pso)
+                       fs=fs_pca or fs_mrmr or fs_pso or fs_gwo)
 
     if fs_pca:
-        build_dataset_with_pca(participant_list=range(1, 33), components=20)
+        build_dataset_with_pca(participant_list=range(1, 33), components=20, classify_type=classify_type)
     elif fs_mrmr:
         use_mrmr(participant_list=range(1, 33), components=20, classify_type=classify_type)
     elif fs_pso:
         use_pso(participant_list=range(1, 33), classify_type=classify_type, n_particle=32, n_iterations=10,
                 components=20)
+    elif fs_gwo:
+        use_gwo(participant_list=range(1, 33), classify_type=classify_type, n_particle=32, n_iterations=10,
+                components=20)
     else:
         build_dataset(participant_list=range(1, 33))
 
-    x_train, y_train, x_test, y_test = prepare_dataset(classify_name, pca=fs_pca, mrmr=fs_mrmr, pso=fs_pso)
+    x_train, y_train, x_test, y_test = prepare_dataset(classify_name, pca=fs_pca, mrmr=fs_mrmr, pso=fs_pso, gwo=fs_gwo)
 
     print("Training: ", x_train.shape, y_train.shape)
     print("Test: ", x_test.shape, y_test.shape)
